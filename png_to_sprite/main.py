8e9f927b8ac60c6d6af696c42f30af784ac4e12d
gametekker <104738069+gametekker@users.noreply.github.com>
2023-01-09 21:16:29 -0500

    Update main.py

--- png_to_sprite/main.py
+++ png_to_sprite/main.py
@@ -1,10 +1,10 @@
 import imageio.v2 as iio
 def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
-img=iio.imread("sprite_name.png")
+img=iio.imread("s.png")
 u_offset = 0
 v_offset = 0
-print("int sprite_name[“+str(img.shape[0]*img.shape[1])+"] = {")
+print("int sprite_name["+str(img.shape[0]*img.shape[1])+"] = {")
 for c in img:
     v_offset=0
     for p in c:
