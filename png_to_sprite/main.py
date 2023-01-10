9d8f0ea8c26535f2c78edd8c6afa511f4cac173d
gametekker <104738069+gametekker@users.noreply.github.com>
2023-01-09 21:00:11 -0500

    Add files via upload

---
+++ png_to_sprite/main.py
@@ -0,0 +1,18 @@
+import imageio.v2 as iio
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
+img=iio.imread("sprite_name.png")
+u_offset = 0
+v_offset = 0
+print("int sprite_name[“+str(img.shape[0]*img.shape[1])+"] = {")
+for c in img:
+    v_offset=0
+    for p in c:
+        R = p[0]+0
+        G = p[1]+0
+        B = p[2]+0
+        print("0x"+str(rgb_to_hex((R,G,B))+","))
+        v_offset+=1
+    u_offset+=1
+print("};")
+print("uLCD.BLIT(3+u, 15+v, "+str(img.shape[1])+", "+str(img.shape[0])+", colors);")
